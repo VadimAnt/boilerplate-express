dc9d0b39d30b7cf2405be2f6934d16eefe47954b
Vadym Antropov <vadim.antropov@da-14.com>
2018-09-06 13:24:18 +0300

    feat: refactoring and added errors classes

---
+++ app/errors/index.js
@@ -0,0 +1,15 @@
+const AlredyExist = require('./AlredyExist');
+const BaseError = require('./BaseError');
+const BadRequest = require('./BadRequest');
+const Forbidden = require('./Forbidden');
+const NotFound = require('./NotFound');
+const Unauthorized = require('./Unauthorized');
+
+module.exports = {
+  AlredyExist,
+  BadRequest,
+  BaseError,
+  Forbidden,
+  NotFound,
+  Unauthorized,
+};
---
+++ app/routes/api/v1/auth/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { AuthService } = require('@services');
+const { AuthController } = require('@controllers');
+
+router.post('/signin', AuthService.verify('local'), AuthController.signin);
+router.post('/logout', AuthService.verify(), AuthController.logout);
+router.post('/signup', AuthController.signup);
+
+module.exports = router;
