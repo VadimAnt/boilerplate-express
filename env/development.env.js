e962f75662af1e2f3d8c2782ba5789f9e90499df
Vadym Antropov <vadim.antropov@da-14.com>
2018-09-06 15:46:10 +0300

    feat: refactoring and added utils

---
+++ env/development.env.js
@@ -0,0 +1,13 @@
+module.exports = {
+  NODE_ENV: 'development',
+  APP_PORT: 8080,
+
+  DB_HOST: 'ds229450.mlab.com',
+  DB_PORT: '29450',
+  DB_USER: 'user',
+  DB_PASSWORD: 'test123456789',
+  DB_DATABASE: 'bolierplate',
+  DB_DIALECT: 'mongodb',
+
+  JWT_SECRET: 'test123456789',
+};
