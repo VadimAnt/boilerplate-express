f9a1802b8929c51bf95c7bb3b04f75c587800728
Vadym Antropov <vadim.antropov@da-14.com>
2018-09-06 15:46:03 +0300

    feat: refactoring and added utils

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 
+const PATH_ENV = './env';
+const commonEnv = require(`${PATH_ENV}/common.env`);
+
 const envirements = ['development', 'production', 'test'];
 
 const config = {
   name: 'bpolierplate-express',
   script: './bin/app.js',
   watch: true,
+  env: commonEnv,
 };
 
 envirements.forEach((env) => {
-  const path = `./config/${env}.env.js`;
+  const path = `./env/${env}.env.js`;
   const envName = `env_${env}`;
   if (fs.existsSync(path)) {
     try {
