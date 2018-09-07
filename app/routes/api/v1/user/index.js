016b9382941bd445390ee8b8698dc9547b2dc998
Vadim <webdevelopers52@gmail.com>
2018-09-07 19:44:39 +0300

    feat: refactor required library

--- app/routes/api/v1/user/index.js
+++ app/routes/api/v1/user/index.js
@@ -2,8 +2,22 @@ const router = require('express').Router();
 const { UserController } = require('@controllers');
 const { AuthService } = require('@services');
 
-router.get('/', AuthService.verify(), UserController.getAll);
-router.get('/:id', AuthService.verify(), UserController.getOne);
-router.post('/', AuthService.verify(), UserController.create);
+router.get(
+  '/',
+  AuthService.isAuthenticated(),
+  UserController.getAll,
+);
+
+router.get(
+  '/:id',
+  AuthService.isAuthenticated(),
+  UserController.getOne,
+);
+
+router.post(
+  '/',
+  AuthService.isAuthenticated(),
+  UserController.create,
+);
 
 module.exports = router;
