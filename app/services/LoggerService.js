a3ad077c5efd2b3981a2a8b6f7af4666adf214f2
Vadym Antropov <vadim.antropov@da-14.com>
2018-09-07 16:34:02 +0300

    feat: added logger

--- app/services/LoggerService.js
+++ app/services/LoggerService.js
@@ -6,17 +6,20 @@ class LoggerService {
     this.logger = provider.createLogger({
       level: 'info',
       format: provider.format.json(),
-      transports: [
-        new (provider.transports.DailyRotateFile)({
-          dirname: 'logs',
-          filename: 'application-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-        }),
-      ],
+      transports: [],
     });
+    this.addTransports();
+  }
+
+  addTransports() {
+    this.logger.add(new (provider.transports.DailyRotateFile)({
+      dirname: 'logs',
+      filename: 'application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }));
   }
 
   log({ message, level = 'info' }) {
