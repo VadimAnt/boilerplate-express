c4090dd40d0fa9b82fe3964ddb0c868127f31d8d
Vadim <webdevelopers52@gmail.com>
2018-09-09 12:02:35 +0300

    feat: added e2e tests

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.use('/api', (req, res) => {
+  res.send({
+    success: true,
+    data: { message: 'Everything alright I work =)' },
+  });
+});
 router.use('/api/v1', require('@routes/api/v1'));
 
 module.exports = router;
